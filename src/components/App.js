cd46c609323afe7b826121edaae3af5087621d68
LeightonSkervin <lskervin23@gmail.com>
2024-02-22 14:20:44 -0500

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import {username, city, image} from "../data/user"
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city}/>
+      <About image={image}/>
     </div>
   );
 }
